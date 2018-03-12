6ad970b28d9139afac3a3707d8d4a43dd16d4fe6
fateevv85 <fateevv85@gmail.com>
2018-03-12 21:42:39 +0300

    finish index.html cart

--- app/js/add_to_cart.js
+++ app/js/add_to_cart.js
@@ -9,43 +9,119 @@
     $.get({
       url: endPoint,
       beforeSend: function () {
-        $('.gooditem').toggleClass('showcase__loading');
+        $('.gooditem').addClass('showcase__loading');
       }
     }).done(function (cart) {
-      $('.gooditem').toggleClass('showcase__loading');
-      Object.keys(cart).forEach(function (value) {
-        $('.gooditem[productId="' + this[value].productId + '"]').addClass('incart');
-      }, cart)
+      $('.shp_cart__loading').hide();
+      //refresh items in showcase
+      refreshShowcase(cart);
+      //render items in cart
+      renderCart(cart);
     });
 
     //add item to the cart by click on "add to cart" button
     $('.addtocart_button').click(function () {
-      var thisButton = $(this);
-
-      if (thisButton.parent().hasClass('incart')) {
+      var button = $(this);
+      if (button.parent().hasClass('incart')) {
         console.log('Already in cart!');
       } else {
+        //send data to json-server
         $.post({
           url: endPoint,
           beforeSend: function () {
-            thisButton.parent().toggleClass('showcase__loading');
+            button.parent().toggleClass('showcase__loading');
           },
           data: {
-            productId: $(this).parent().attr('productId'),
-            price: $(this).next().children('.item_cost').text(),
-            imgURL: $(this).prev().css('background-image'),
-            productName: $(this).next().children(':first').text()
+            productId: button.parent().attr('productId'),
+            price: button.next().children('.item_cost').text(),
+            imgURL: button.prev().css('background-image'),
+            productName: button.next().children(':first').text()
           }
         }).done(function () {
-          console.log('Success!');
-          thisButton.parent().addClass('incart').toggleClass('showcase__loading');
-        })
-          .fail(function () {
-            console.log('Error!');
-          });
+          button.parent().addClass('incart').toggleClass('showcase__loading');
+          refreshCart();
+        });
       }
     });
 
-    //end of jQuery
-  });
+    //render function
+    function renderCart(cart) {
+      //if cart has items
+      if (cart.length) {
+        var totalCost = 0;
+        //create items elements
+        for (var i = 0; i < cart.length; i++) {
+          var itemInCart = $('<div />')
+              .addClass('gooditem_incart')
+              .attr({
+                productId: cart[i].productId,
+                id: cart[i].id
+              }),
+            img = $('<div />')
+              .addClass('image')
+              .css('background-image', cart[i].imgURL),
+            //remove item by click on cross
+            cross = $('<div />')
+              .addClass('item_cross')
+              .click(deleteItem),
+            name = $('<div />').text(cart[i].productName),
+            itemCost = $('<p />').addClass('item_cost').text(cart[i].price),
+            a = $('<a href=# />').append(name, itemCost, $('<div >'));
+          itemInCart.append(img, cross, a);
+          $('.shp_cart__list').append(itemInCart);
+          //calculate total cost
+          totalCost += parseInt(cart[i].price);
+        }
+        $('.shp_cart__total').show().children('span').text(totalCost);
+        //additional buttons are visible
+        $('.shp_cart__checkout, .shp_cart__gotocart').show();
+        //number of items indicator
+        $('.items_number').show().text(cart.length);
+      } else {
+        $('.shp_cart__empty').show();
+        $('.shp_cart__checkout, .shp_cart__gotocart').hide();
+        $('.items_number').hide();
+        $('.shp_cart__total').hide();
+      }
+    }
+
+    //refresh cart then deleting or adding items
+    function refreshCart() {
+      $.get({
+        url: endPoint
+      }).done(function (cart) {
+        $('.shp_cart__list').empty();
+        //render items in cart
+        renderCart(cart);
+        refreshShowcase(cart);
+        $('.shp_cart__list').removeClass('cart__list_loading');
+      });
+    }
+
+    function refreshShowcase(cart) {
+      $('.gooditem').removeClass('showcase__loading incart');
+
+      console.log(cart);
+      //if item is in cart, marking it
+      Object.keys(cart).forEach(function (value) {
+        $('.gooditem[productId="' + this[value].productId + '"]').addClass('incart');
+      }, cart);
+    }
+
+    //delete item in cart
+    function deleteItem() {
+      $.ajax({
+        method: 'DELETE',
+        url: endPoint + '/' + $(this).parent().attr('id'),
+        beforeSend: function () {
+          $('.shp_cart__list').addClass('cart__list_loading');
+        }
+      }).done(function () {
+          refreshCart();
+        }
+      );
+    }
+
+
+  });  //end of jQuery
 })(jQuery);
