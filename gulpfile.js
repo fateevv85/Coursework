1ce5659adc4813d5c6a51be6d972a5d1a0700f5e
fateevv85 <fateevv85@gmail.com>
2018-03-13 16:57:49 +0300

    Add task with images

--- gulpfile.js
+++ gulpfile.js
@@ -7,36 +7,51 @@ var gulp = require('gulp'),
     minifyCSS = require('gulp-minify-css'),
     imagemin = require('gulp-imagemin'),
     del = require('del'),
-    runSequence = require('run-sequence');
+    runSequence = require('run-sequence'),
+    cache = require('gulp-cache');
 
 gulp.task('pressFiles', function () {
   return gulp.src('app/*.html')
     .pipe(useref())
     .pipe(gulpif('*.js', uglify()))
     .pipe(gulpif('*.css', minifyCSS()))
+    // .pipe(gulpif('*.css', minifyCSS({processImport: false})))
     .pipe(gulpif('*.css', autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     })))
     .pipe(gulp.dest('dist'))
-    .pipe(notify('Done!'));
+    // .pipe(notify('Done!'));
 });
 
 gulp.task('images', function () {
   gulp.src('app/img/**/*.+(png|jpg|jpeg|gif|svg)')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
+  // кэширование изображений, прошедших через imagemin
+    .pipe(cache(imagemin({
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/img'))
 });
 
 gulp.task('clean', function () {
-  del('dist');
+  del('dist/**/*');
 });
 
-gulp.task('clean:dist', function () {
+gulp.task('cleanNotImg', function () {
   del(['dist/**/*', '!dist/img', '!dist/img/**/*']);
 });
 
+gulp.task('fonts', function () {
+  return gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('libs', function () {
+  return gulp.src('app/libs/**/*')
+    .pipe(gulp.dest('dist/libs'))
+});
+
 gulp.task('build', function () {
-  runSequence('clean', ['pressFiles', 'images']);
+  runSequence('cleanNotImg', ['pressFiles', 'images', 'fonts', 'libs']);
 });
 
